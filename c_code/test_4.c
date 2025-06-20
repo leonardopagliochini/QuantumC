02615793f3407fd440cba8a66f86df398cd2d479
Francesco Rosnati <145071471+RosNaviGator@users.noreply.github.com>
2025-06-20 10:39:40 +0200

    Mix immediate ops into C tests

--- c_code/test_4.c
+++ c_code/test_4.c
@@ -1,7 +1,10 @@
 int main() {
     int a = 1, b = a + 4, c = a + b;
     int d = c + 2;
-    int e = d * b * (a + 1);
+    int e = d * b * (a + 1) + 3;
     int f = e / (d + 1);
+    f = f + 10;
+    int g = f - 5;
+    g = g * 2;
     return 0;
 }
