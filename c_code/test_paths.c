348c82927b9ce2d2c2a596aa9b2beb0ed04d00e0
RosNaviGator <borndescalo@gmail.com>
2025-06-21 20:28:35 +0200

    added test paths

---
+++ c_code/test_paths.c
@@ -0,0 +1,36 @@
+int main() {
+    int a = 1;
+    int x = 2;
+    int b = a + x + 5;
+
+    int y = 3;
+    int c = (b * y) * 2;
+
+    int d = a + x - 1;
+
+    int e = c * d;
+
+    int z = 4;
+    int f = z * a + 3;
+
+    int g = f * e;
+    int h = a + c;
+
+    int w = 7;
+    int i = a + w;
+
+    int j = e + a;
+    int k = (e + c) / 2;
+
+    int l = 9;
+    int m = l - j;
+
+    int n = h + i + 10;
+    int o = g - d - 6;
+    int p = j * x * 3;
+    int q = p / y / 2;
+    int r = q + a + 1;
+    int s = r - c - 2;
+
+    return s;
+}
